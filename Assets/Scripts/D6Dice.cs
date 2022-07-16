1fe968041532dff9d876c0a150d4bbac08e3d8e1
hkeane <hkeane@alumni.usc.edu>
2022-07-16 16:34:15 -0700

    Reorganized Project

---
+++ Assets/Scripts/D6Dice.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D6Dice : MonoBehaviour
+{
+    [SerializeField]
+    private Transform side1;
+    [SerializeField]
+    private Transform side2;
+    [SerializeField]
+    private Transform side3;
+    [SerializeField]
+    private Transform side4;
+    [SerializeField]
+    private Transform side5;
+    [SerializeField]
+    private Transform side6;
+
+    private List<Transform> sides;
+
+    private Transform top;
+
+    private Rigidbody rb;
+
+    private Vector3 diceVelocity;
+
+    public Vector3 DiceVelocity { get => diceVelocity; set => diceVelocity = value; }
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        diceVelocity = rb.velocity;
+
+
+        sides = new List<Transform>();
+        sides.Add(side1);
+        sides.Add(side2);
+        sides.Add(side3);
+        sides.Add(side4);
+        sides.Add(side5);
+        sides.Add(side6);
+
+        //assign top to a random side
+        top = side1;
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        diceVelocity = rb.velocity;
+    }
+
+    public Transform FindTopside()
+    {
+        foreach (Transform side in sides)
+        {
+            if(side.position.y > top.position.y)
+            {
+                top = side;
+            }
+        }
+        return top;
+    }
+
+
+    void MeBug(string toLog)
+    {
+        Debug.Log(gameObject.name + " / " + toLog);
+    }
+
+}
---
+++ Assets/Scripts/RandomForceOnStart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomForceOnStart : MonoBehaviour
+{
+
+    Rigidbody m_Rigidbody;
+    [SerializeField]
+    private float m_Thrust;
+    // Start is called before the first frame update
+
+
+
+
+
+
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+
+        //Rng roll dice
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+        float rngJump = Random.Range(0, m_Thrust);
+        m_Rigidbody.AddForce(transform.up * rngJump);
+
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
