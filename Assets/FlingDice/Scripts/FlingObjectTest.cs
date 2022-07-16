704b3c7b2c4849a04296f95be4931f4039055b50
Nickymonkey <nickroewe@gmail.com>
2022-07-16 15:13:43 -0700

    Fling Object Basics

---
+++ Assets/FlingDice/Scripts/FlingObjectTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlingObjectTest : MonoBehaviour
+{
+    float xroat, yroat = 0f;
+    public Rigidbody ball;
+    public float rotatespeed = 5f;
+    public LineRenderer line;
+    public float shootpower = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ball.position;
+        if (Input.GetMouseButton(0))
+        {
+            xroat += Input.GetAxis("Mouse X") * rotatespeed;
+            yroat += Input.GetAxis("Mouse Y") * rotatespeed;
+            transform.rotation = Quaternion.Euler(yroat, xroat, 0f);
+            line.gameObject.SetActive(true);
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, transform.position + transform.forward * 4f);
+            if (yroat < -35f)
+            {
+                yroat = -35f;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            ball.velocity = transform.forward * shootpower;
+            line.gameObject.SetActive(false);
+        }
+
+    }
+}
