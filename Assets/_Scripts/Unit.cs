9fe666131fe79b090fe3fb947c687acde0d30a35
ptaggs <ptaggs@gmail.com>
2022-07-17 02:38:49 -0700

    Clean up unit class
    Remove double declarations only use the stat struct.
    
    Update values on prefabs to allow for proper combat.

--- Assets/_Scripts/Unit.cs
+++ Assets/_Scripts/Unit.cs
@@ -6,14 +6,12 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Unit : MonoBehaviour
 {
-    private NavMeshAgent navAgent;
-    public float aggroRange, atkRange, atkSped, attack, health, armor;
     public UnitStatTypes.Base stats;
 
     public void UpdateHealth(float delta)
     {
-        health += delta;
-        if (health <= 0)
+        stats.health += delta;
+        if (stats.health <= 0)
         {
             Die();
         }
